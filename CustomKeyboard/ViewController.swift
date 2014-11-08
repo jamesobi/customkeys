9835f4c65527fe67ca10f852dc3419b39b48d296
James Sobieski <ringoandhula@gmail.com>
2014-11-08 13:20:14 -0800

    Initial Commit

---
+++ CustomKeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomKeyboard
+//
+//  Created by James Sobieski on 11/8/14.
+//  Copyright (c) 2014 James S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
